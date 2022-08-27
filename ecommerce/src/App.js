3ca510b29cea0cbf59f4a8fb66c85dd4bd97815f
cynamon jules <julescweb@mail.com>
2022-08-27 13:14:00 +0200

    mise en place

---
+++ ecommerce/src/App.js
@@ -0,0 +1,16 @@
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Homepage from "./Pages/homepage/Homepage";
+
+
+function App() {
+	return (
+		<>
+			<Header />
+			{/* <Homepage />
+			<Footer /> */}
+		</>
+	);
+}
+
+export default App;
---
+++ ecommerce/src/components/footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <div class="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+            Coded by <a href="https://julescynamon.github.io/cv_portfolio/">Jules Cynamon</a>.
+        </div>
+    )
+}
